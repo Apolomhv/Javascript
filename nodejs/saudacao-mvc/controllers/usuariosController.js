0f9554bb25cf448cc7cf22fee3db4b587033dd6d
Apolo Miguel Hernández Vásquez <apoloh768@gmail.com>
2025-10-24 16:55:22 -0300

    Login (não funcionando)

---
+++ nodejs/saudacao-mvc/controllers/usuariosController.js
@@ -0,0 +1,17 @@
+const usuariosModel = require ('../models/usuariosModel');
+
+module.exports = {
+    index: (req, res) => {
+        res.sendFile('usuarios.html', { root: './views' });
+    },
+
+    formulario: (req, res) => {
+        res.sendFile('formLogin.html', { root: './views' });
+    },
+
+    login: (req, res) => {
+        const { login, senha } = req.body;
+        const mensagemAcesso = usuariosModel.verficairLogin(login, senha);
+        res.send(`<h1>${mensagemAcesso}</h1>`);
+    }
+};
