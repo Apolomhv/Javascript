b3ac06a273d3ece5de1dd253f7fe752fea693b66
Apolo Miguel Hernández Vásquez <apoloh768@gmail.com>
2025-10-28 09:31:40 -0300

    Versão com cadastro de protudo

---
+++ nodejs/saudacao-mvc/controllers/produtosController.js
@@ -0,0 +1,22 @@
+const produtosModel = require('../models/produtosModel');
+const { camisetas } = require('./siteController');
+
+module.exports = {
+    produtos: (req, res) => {
+    res.sendFile('produtos.html', { root: './views' });
+    },
+
+    camisetas: (req, res) => {
+        res.sendFile('camisetas.html', { root: './views' });
+    },
+
+    formCadastrar: (req, res) => {
+        res.sendFile('formCadastrar.html', { root: './views' });
+    },
+
+    cadastrar: (req, res) => {
+        const { id, descricao, quantidade, preco } = req.body;
+        const mensagemCadastro = produtosModel.cadastrar(id, descricao, quantidade, preco);
+        res.send(`<h1>${mensagemCadastro}</h1>`);
+    }
+};
---
+++ nodejs/saudacao-mvc/models/produtosModel.js
@@ -0,0 +1,7 @@
+module.exports = {
+
+    cadastrar: (id, descricao, quantidade, preco) => {
+
+        return `O produto ${descricao}, com ID: ${id} - QUantidade: ${quantidade} - Valor Unitário: ${precp} foi cadastrado com sucesso!`;
+    }
+};
